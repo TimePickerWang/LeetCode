6e597695f7d4382e0b79713461606043a4441b61
FREEMAN <454592297@qq.com>
2018-10-03 13:16:48 +0800

    016

---
+++ LeetCode016.java
@@ -0,0 +1,34 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class LeetCode016 {
+	public static int threeSumClosest(int[] nums, int target) {
+		Arrays.sort(nums);
+		int dist = Integer.MAX_VALUE;
+		int result = Integer.MAX_VALUE;
+		for (int i = 0; i < nums.length - 2; i++) {
+			int left = i + 1, right = nums.length - 1;
+			while (left < right) {
+				int sum = nums[i] + nums[left] + nums[right];
+				if (sum < target) {
+					left++;
+				} else if (sum > target) {
+					right--;
+				} else {
+					return sum;
+				}
+				if (Math.abs(sum - target) < dist) {
+					dist = Math.abs(sum - target);
+					result = sum;
+				}
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 0, 2, 1, -3 };
+		System.out.println(threeSumClosest(nums, 0));
+	}
+}
