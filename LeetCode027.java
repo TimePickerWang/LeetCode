6aa5ea900843e6fb0efa76c6fa002129b64ea826
FREEMAN <454592297@qq.com>
2018-10-14 22:37:57 +0800

    027

---
+++ LeetCode027.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+public class LeetCode027 {
+	public static int removeElement(int[] nums, int val) {
+		if (nums.length == 0 || nums == null) {
+			return 0;
+		}
+		int left = 0, right = nums.length - 1;
+		while (left < right) {
+			while (left < right && nums[right] == val) {
+				right--;
+			}
+			while (left < right && nums[left] != val) {
+				left++;
+			}
+			if (left < right) {
+				nums[left] = nums[right--];
+			}
+		}
+		return nums[0] == val ? 0 : left + 1;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 3, 3 };
+		int val = 3;
+		System.out.println(removeElement(nums, val));
+	}
+}
