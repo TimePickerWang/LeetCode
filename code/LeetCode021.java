a60cefc72832c594dbc9be159700aa3e2aef51fb
FREEMAN <454592297@qq.com>
2019-02-24 19:51:11 +0800

    Update LeetCode021.java

--- code/LeetCode021.java
+++ code/LeetCode021.java
@@ -20,22 +20,4 @@ public class LeetCode021 {
 		}
 		return head.next;
 	}
-
-	public static void main(String[] args) {
-		ListNode a1 = new ListNode(1);
-		ListNode a2 = new ListNode(2);
-		ListNode a3 = new ListNode(4);
-		a1.next = a2;
-		a2.next = a3;
-		ListNode b1 = new ListNode(1);
-		ListNode b2 = new ListNode(3);
-		ListNode b3 = new ListNode(4);
-		b1.next = b2;
-		b2.next = b3;
-		ListNode head = mergeTwoLists(a1, b1);
-		while (head != null) {
-			System.out.print(head.val + "->");
-			head = head.next;
-		}
-	}
 }
