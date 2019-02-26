9a622f2d8eda9f2ac2591a4622781ae89b6860a1
FREEMAN <454592297@qq.com>
2019-02-26 20:55:38 +0800

    Update LeetCode024.java

--- code/LeetCode024.java
+++ code/LeetCode024.java
@@ -10,20 +10,4 @@ public class LeetCode024 {
 		nextNode.next = head;
 		return nextNode;
 	}
-
-	public static void main(String[] args) {
-		ListNode a = new ListNode(1);
-		ListNode b = new ListNode(2);
-		ListNode c = new ListNode(3);
-		ListNode d = new ListNode(4);
-		a.next = b;
-		b.next = c;
-		c.next = d;
-
-		ListNode node = swapPairs(a);
-		while (node != null) {
-			System.out.println(node.val + "->");
-			node = node.next;
-		}
-	}
 }
